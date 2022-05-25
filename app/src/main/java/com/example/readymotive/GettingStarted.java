826a65014116f2bdff309bd0f3f73ade44359258
aunuj <aunujain53@gmail.com>
2022-05-25 12:48:16 +0530

    sign in activity

--- app/src/main/java/com/example/readymotive/GettingStarted.java
+++ app/src/main/java/com/example/readymotive/GettingStarted.java
@@ -2,13 +2,29 @@ package com.example.readymotive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GettingStarted extends AppCompatActivity {
+
+    TextView singInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_started);
+
+        //refrences
+        singInButton = findViewById(R.id.sign_in_button);
+
+        singInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GettingStarted.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/readymotive/SignInActivity.java
+++ app/src/main/java/com/example/readymotive/SignInActivity.java
@@ -3,12 +3,27 @@ package com.example.readymotive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    ImageView cutImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //refrences
+
+        cutImageButton = findViewById(R.id.cut_imageButton);
+
+        cutImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
